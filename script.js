6a4a96a8887f29f4dd651288067dfb2fcdeb3f7a
lyleaigbedion <Lyle-Anthony.Aigbedion75@myhunter.cuny.edu>
2020-01-22 01:37:11 -0500

    linked click to board array

--- script.js
+++ script.js
@@ -10,12 +10,30 @@ const gameBoard = (()=>{
             for(let j = 0; j < 3; j++){
                 let box = document.createElement('div');
                 box.className = 'box';
-                box.setAttribute("data-index", `row: ${i}, cell: ${j}`);//string.split(",")then number that ish
+                box.setAttribute("data-index", `row: ${i}, cell: ${j}`);//string.split(",") before regex then number that ish
                 box.innerHTML = `${board[i][j]}`;
                 row.appendChild(box);
             }
             document.getElementById('gameboard').appendChild(row);
         }
+        let allBoxes = document.querySelectorAll('[data-index]');//node ist of all squares
+        const regex = /[0-9]/gi;
+        for(const boxes of allBoxes){
+            boxes.addEventListener('click', function(){
+                let numArr = this.dataset.index.match(regex);
+                numArr.forEach((el,index,array) =>{
+                    array[index] = Number(el);//just converting the el is not enough, you're renaming the value AT the address. 
+                });
+                /*for(let i = 0; i < 2; i++){
+                    numArr[i] = Number(numArr[i]);
+                }*/
+                console.log(numArr);
+                board[numArr[0]][numArr[1]] = "GAH";
+                console.log(board)
+                
+            });
+        }
+
     }
     return{
         makeBoard
